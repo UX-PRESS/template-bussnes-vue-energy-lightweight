4311319f403dab0de45ed7a04a5e0d7dd500a2af
Blackwidow <work.luanacordeiro@gmail.com>
2025-08-14 12:50:42 -0300

    feat(solutioncard): fixing the solutions card size

--- src/components/SolucionsSection.vue
+++ src/components/SolucionsSection.vue
@@ -1,15 +1,15 @@
 <template>
   <section
-    class="relative -top-70 min-h-screen bg-cover bg-center flex items-center justify-center px-6 sm:px-12 z-0"
-    style="background-image: url('/light.png');"
-  >
-    <div class="w-full flex justify-center">
+    class="relative lg:-top-70 min-h-screen bg-cover bg-center flex items-center justify-center px-6 sm:px-12 z-0"
+    style="background-image: url('/light.png');">
+
+    <div class=" w-full flex justify-center">
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
-        <div
-          v-for="(card, index) in cards"
+        <div v-for="(card, index) in cards"
           :key="index"
-          class="w-90 h-90 rounded-full bg-[var(--amber-cream)] shadow-lg flex flex-col items-center justify-center text-center p-6 hover:scale-105 transition-all duration-300"
-        >
+          class="card w-60 h-60 rounded-full bg-[var(--amber-cream)] shadow-lg flex flex-col items-center justify-center 
+          text-center p-6 hover:scale-105 transition-all duration-300">
+
           <h1 class="text-lg font-semibold text-[var(--graphite-black)] mb-2">
             {{ card.title }}
           </h1>
